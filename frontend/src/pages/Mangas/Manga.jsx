a0747958a2a2410944c129f0a6866662cf486c2c
alexspector123 <alexspector8766@gmail.com>
2025-09-21 10:13:06 +0700

    fix: fix bug

--- frontend/src/pages/Mangas/Manga.jsx
+++ frontend/src/pages/Mangas/Manga.jsx
@@ -21,7 +21,7 @@ import { LuFlag } from "react-icons/lu";
 import { FiUpload } from "react-icons/fi";
 import { GoDotFill } from "react-icons/go";
 
-import MangaTagSection from "./MangaTagSection.jsx";
+import MangaTagSection from "./MangaTagSection";
 import ArtGallery from "./ArtGallery.jsx";
 import MangaChapterSection from "./MangaChapterSection.jsx";
 
