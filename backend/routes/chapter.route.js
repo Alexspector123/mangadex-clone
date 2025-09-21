650bf07ca7171fd1d2138abeff13adaca3f56f60
alexspector123 <alexspector8766@gmail.com>
2025-09-21 17:26:15 +0700

    fix

--- backend/routes/chapter.route.js
+++ backend/routes/chapter.route.js
@@ -1,16 +1,22 @@
-import express from 'express';
-import uploadChapter from '../middlewares/uploadChapter.js';
-import { fetchChapterList, fetchChapterByID, fetchChaptersBatch, fetchChapterReader, addChapter} from '../controllers/chapter.controller.js';
+import express from "express";
+import uploadChapter from "../middlewares/uploadChapter.js";
+import {
+  fetchChapterList,
+  fetchChapterByID,
+  fetchChaptersBatch,
+  fetchChapterReader,
+  addChapter,
+} from "../controllers/chapter.controller.js";
 
 const router = express.Router();
 
 // Route to fetch chapters
 
-router.get('/reader/:id', fetchChapterReader);
-router.post('/upload', uploadChapter.fields([{ name: "pages", maxCount: 150 }]), addChapter);
-router.post('/batch', fetchChaptersBatch);
+router.get("/reader/:id", fetchChapterReader);
+//router.post('/upload', uploadChapter.fields([{ name: "pages", maxCount: 150 }]), addChapter);
+router.post("/batch", fetchChaptersBatch);
 
-router.get('/', fetchChapterList);
+router.get("/", fetchChapterList);
 
-router.get('/:id', fetchChapterByID);
+router.get("/:id", fetchChapterByID);
 export default router;
--- backend/routes/manga.route.js
+++ backend/routes/manga.route.js
@@ -1,13 +1,19 @@
-import express from 'express';
-import uploadManga from '../middlewares/uploadManga.js';
-import { fetchMangaList, fetchMangaById, fetchVolumeListByID, searchManga, addManga } from '../controllers/manga.controller.js';
+import express from "express";
+import uploadManga from "../middlewares/uploadManga.js";
+import {
+  fetchMangaList,
+  fetchMangaById,
+  fetchVolumeListByID,
+  searchManga,
+  addManga,
+} from "../controllers/manga.controller.js";
 
 const router = express.Router();
 
-router.get('/', fetchMangaList); // /api/manga?params=...
-router.get('/volume/:id', fetchVolumeListByID);
-router.get('/search', searchManga);
-router.post('/upload', uploadManga.single("image"), addManga);
-router.get('/:id', fetchMangaById);
+router.get("/", fetchMangaList); // /api/manga?params=...
+router.get("/volume/:id", fetchVolumeListByID);
+router.get("/search", searchManga);
+//router.post('/upload', uploadManga.single("image"), addManga);
+router.get("/:id", fetchMangaById);
 
 export default router;
